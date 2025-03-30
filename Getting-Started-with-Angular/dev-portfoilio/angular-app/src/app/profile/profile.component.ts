aaa4dde2c9b8c830828ea09bbcad0a1f721db194
cloudshare360 <79122392+cloudshare360@users.noreply.github.com>
2025-03-30 05:42:15 +0000

    added dev-portfolio

---
+++ Getting-Started-with-Angular/dev-portfoilio/angular-app/src/app/profile/profile.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  standalone: true,
+  imports: [],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  name: string = "Srinivasa Rao Gurram";
+  role: string = "Software Developer";
+  location: string = "Dallas, USA";
+  linkedinUrl: string = "https://www.linkedin_profile_url.com";
+  githubUrl: string = "https://github_profile_url.com";
+}
